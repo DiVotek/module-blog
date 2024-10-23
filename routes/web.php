5edb2f53cd35daed8cc530cc1cd0d1ab248732cb
maxboyko <maxboiko@divotek.com>
2024-10-23 13:01:16 +0300

    fix for routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\StaticPage;
 use App\Models\SystemPage;
 use Illuminate\Support\Facades\Route;
 use Modules\Blog\Controllers\BlogController;
+use App\Services\MultiLang;
 
 if (!function_exists('blog_slug')) {
    function blog_slug()
@@ -15,3 +16,10 @@ if (!function_exists('blog_slug')) {
 
 Route::get(blog_slug() . '/{category}', [BlogController::class, 'category'])->name('blog-category');
 Route::get(blog_slug() . '/{category}/{article}', [BlogController::class, 'article'])->name('blog-article');
+
+if (is_multi_lang()) {
+   foreach (MultiLang::getActiveLanguages() as $language) {
+      Route::get($language->slug . '/'. blog_slug() . '/{category}', [BlogController::class, 'category'])->name($language->slug . '.blog-category');
+      Route::get($language->slug . '/'. blog_slug() . '/{category}/{article}', [BlogController::class, 'article'])->name($language->slug . '.blog-article');
+   }
+}
